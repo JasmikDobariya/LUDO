5f3c19b86c78948a6c6995135d0deec1b3f992da
jasmin <jasmikdobariya@gmail.com>
2024-03-21 13:56:05 +0530

    fix dig

--- app/header/header.tsx
+++ app/header/header.tsx
@@ -7,10 +7,10 @@ export default function Header() {
   return (
     <section>
 
-      <div className='flex  '>
-        <div className='w-[480px] h-full  top-0  '>
-          <div className='  z-10 bg-white '>
-            <div className=' border-r-2 border-black px-5 shadow-lg  flex justify-between items-center'>
+      <div className='flex md:absolute'>
+        <div className='w-[480px] top-0'>
+          <div className='bg-white'>
+            <div className=' border-r-2 border-black px-5 py-3 shadow-lg  flex justify-between items-center'>
               <div>
                 <Image src={logo} width={60} height={60} alt='/' />
               </div>
--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
       try {
         await installPrompt.prompt();
         const choiceResult = await installPrompt.userChoice;
-        console.log(choiceResult); 
+        console.log(choiceResult);
       } catch (error) {
         console.error('Error prompting installation:', error);
       }
@@ -76,40 +76,38 @@ export default function Home() {
 
 
   return (
-    <><Header /><section>
-
-      <div className=''>
-
-        <div className='flex h-screen'>
-          <div className='w-[480px] h-full p-4 border-r-2 border-black relative overflow-auto'>
-            <div className='text-18px font-semibold py-4'>
-              <h1>Our Tournaments</h1>
-            </div>
-            <div className='grid grid-cols-2 2xl:grid-cols-2 gap-4'>
-              <div>
-                <Image src={logo} width={210} height={210} alt='/' />
+    <>
+      <Header />
+      <section>
+        <div className='flex'>
+          <div className='w-[480px] p-4 border-r-2 border-black h-screen overflow-y-auto md:pt-20'>
+              <div className='text-18px font-semibold py-4'>
+                <h1>Our Tournaments</h1>
               </div>
-              <div>
-                <Image src={logo} width={210} height={210} alt='/' />
-              </div>
-              <div>
-                <Image src={logo} width={210} height={210} alt='/' />
-              </div>
-              <div>
-                <Image src={logo} width={210} height={210} alt='/' />
-              </div>
-            </div>
-            {/* <div className='slider-container'>
-              <Slider {...settings}>
+              <div className='grid grid-cols-2 2xl:grid-cols-2 gap-4'>
                 <div>
-                  <Image src={logo} alt='/' />
+                  <Image src={logo} width={210} height={210} alt='/' />
                 </div>
                 <div>
-                  <Image src={logo} alt='/' />
+                  <Image src={logo} width={210} height={210} alt='/' />
                 </div>
-              </Slider>
-            </div> */}
-
+                <div>
+                  <Image src={logo} width={210} height={210} alt='/' />
+                </div>
+                <div>
+                  <Image src={logo} width={210} height={210} alt='/' />
+                </div>
+              </div>
+              <div className='slider-container mt-5 md:pb-24 pb-10  '>
+                <Slider {...settings}>
+                  <div>
+                    <Image src={logo} alt='/' />
+                  </div>
+                  <div>
+                    <Image src={logo} alt='/' />
+                  </div>
+                </Slider>
+              </div>
             <div className='fixed bottom-0 left-0 sm:w-[480px] w-full bg-gradient-to-r from-purple-500 to-pink-500'>
               <div className='py-2 rounded-md text-center flex justify-center items-center cursor-pointer' onClick={handleInstallButtonClick}>
                 <button>DOWNLOAD OUR APP</button>
@@ -135,23 +133,17 @@ export default function Home() {
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" className='pl-3'><path d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM504 312V248H440c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V136c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H552v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z" /></svg>
                   <button className="mt-1">Refer</button>
                 </div>
-
-
-
-
-
               </div>
             </div>
           </div>
-          <div className='hidden xl:flex xl:w-[65%] xl:h-full xl:justify-center xl:items-center'>
-            <div className='flex flex-col text-2xl h-full font-semibold items-center justify-center'>
+          <div className='max-xl:hidden flex xl:w-[65%] justify-center items-center'>
+            <div className='flex flex-col text-2xl font-semibold items-center justify-center'>
               <Image src={logo} width={310} height={310} alt='/' />
               <h1>JAIPURLUDO WIN REAL CASH</h1>
             </div>
           </div>
         </div>
-
-      </div>
-    </section></>
+      </section>
+    </>
   );
 }
