1c0589fca08bba74d6ee6db59b448e3bd4900fee
jasmin <jasmikdobariya@gmail.com>
2024-03-22 19:14:20 +0530

    fix header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -6,7 +6,7 @@ import header_logo from '../../public/assets/images/header_logo.png'
 export default function Header({ onLogoClick }: any) {
 
   return (
-    <header className="fixed top-[4px] sm:w-[471px] min-w-[371px] rounded-t-xl  bg-white  shadow-lg">
+    <header className="fixed top-[4px] sm:w-[471px] w-full rounded-t-xl  bg-white  shadow-lg">
       <div className="container mx-auto px-4 py-3 md:py-4 flex justify-center items-center">
         <div className="cursor-pointer" onClick={onLogoClick}>
           <Image src={header_logo} alt="/" />
