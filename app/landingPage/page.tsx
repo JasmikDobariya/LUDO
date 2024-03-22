a26e388d1492b4b34a4e4dd18c7b0f40a5214f58
jasmin <jasmikdobariya@gmail.com>
2024-03-22 13:16:48 +0530

    bottombar fix

--- app/landingPage/page.tsx
+++ app/landingPage/page.tsx
@@ -6,6 +6,7 @@ import Header from '../header/header';
 import logo from '../../public/assets/images/logo.png'
 import Link from 'next/link';
 import { Landingheader } from '../header/landingheader';
+import { BottomBar } from '../bottombar/BottomBar';
 
 export default function LandingPage() {
     let settings = {
@@ -126,27 +127,9 @@ export default function LandingPage() {
                                 {/* <div className='py-2 rounded-md text-center flex justify-center items-center cursor-pointer' onClick={handleInstallButtonClick}>
                                     <button>DOWNLOAD OUR APP</button>
                                 </div> */}
-                                <div className='bg-slate-600 flex justify-between w-full px-8 p-4'>
-                                    <div className="flex flex-col justify-center text-center items-center">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1058 512" className='pl-3'>
-                                            <path fill='#fff' d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                                        </svg>
-                                        <button className="mt-1 text-white text-sm">Profile</button>
-                                    </div>
-                                    <div className="flex flex-col justify-center text-center items-center">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1058 512" className='pl-3'><path fill='#fff' d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" /></svg>
-                                        <button className="mt-1 text-white text-sm">Wallet</button>
-                                    </div>
-                                    <div className="flex flex-col justify-center text-center items-center">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1058 512" className='pl-3'>
-                                            <path fill='#fff' d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                                        </svg>
-                                        <button className="mt-1 text-white text-sm">History</button>
-                                    </div>
-
-                                </div>
-                            </div></div>
-
+                                <BottomBar />
+                            </div>
+                        </div>
                     </div>
                     <div className='max-sm:hidden border-l-4 border-[#cacaca] flex w-[65%] justify-center items-center '>
                         <div className='flex flex-col text-2xl font-semibold items-center justify-center'>
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Header from "./header/header";
 import Slider from "react-slick";
 import { useEffect, useState } from "react";
 import LoginPage from "./loginPage/page";
+import { BottomBar } from "./bottombar/BottomBar";
 
 export default function Home() {
   let settings = {
@@ -48,7 +49,8 @@ export default function Home() {
   };
 
   const [showHome, setShowHome] = useState(true);
-  const [installPrompt, setInstallPrompt] = useState<any>(null);
+
+
 
   const handleLoginClick = () => {
     setShowHome(false);
@@ -57,33 +59,34 @@ export default function Home() {
     setShowHome(true);
   };
 
-  useEffect(() => {
-    const handleBeforeInstallPrompt = (event: any) => {
-      event.preventDefault();
-      setInstallPrompt(event);
-    };
+  // const [installPrompt, setInstallPrompt] = useState<any>(null);
+  // useEffect(() => {
+  //   const handleBeforeInstallPrompt = (event: any) => {
+  //     event.preventDefault();
+  //     setInstallPrompt(event);
+  //   };
 
-    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+  //   window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
 
-    return () => {
-      window.removeEventListener(
-        "beforeinstallprompt",
-        handleBeforeInstallPrompt
-      );
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener(
+  //       "beforeinstallprompt",
+  //       handleBeforeInstallPrompt
+  //     );
+  //   };
+  // }, []);
 
-  const handleInstallButtonClick = async () => {
-    if (installPrompt) {
-      try {
-        await installPrompt.prompt();
-        const choiceResult = await installPrompt.userChoice;
-        console.log(choiceResult);
-      } catch (error) {
-        console.error("Error prompting installation:", error);
-      }
-    }
-  };
+  // const handleInstallButtonClick = async () => {
+  //   if (installPrompt) {
+  //     try {
+  //       await installPrompt.prompt();
+  //       const choiceResult = await installPrompt.userChoice;
+  //       console.log(choiceResult);
+  //     } catch (error) {
+  //       console.error("Error prompting installation:", error);
+  //     }
+  //   }
+  // };
 
   return (
     <>
@@ -134,53 +137,13 @@ export default function Home() {
                   </Slider>
                 </div>
                 <div className="fixed bottom-0 left-0 sm:w-[480px] w-full bg-gradient-to-r from-purple-500 to-pink-500">
-                  <div
+                  {/* <div
                     className="py-2 rounded-md text-center flex justify-center items-center cursor-pointer"
                     onClick={handleInstallButtonClick}
                   >
                     <button>DOWNLOAD OUR APP</button>
-                  </div>
-                  <div className="bg-slate-600 flex justify-between w-full p-4">
-                    <div className="flex flex-col justify-center text-center items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 958 512"
-                        className="pl-3"
-                      >
-                        <path
-                          fill="#fff"
-                          d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"
-                        />
-                      </svg>
-                      <button className="mt-1 text-white">Profile</button>
-                    </div>
-                    <div className="flex flex-col justify-center text-center items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 958 512"
-                        className="pl-3"
-                      >
-                        <path
-                          fill="#fff"
-                          d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"
-                        />
-                      </svg>
-                      <button className="mt-1 text-white">Wallet</button>
-                    </div>
-                    <div className="flex flex-col justify-center text-center items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 958 512"
-                        className="pl-3"
-                      >
-                        <path
-                          fill="#fff"
-                          d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"
-                        />
-                      </svg>
-                      <button className="mt-1 text-white">History</button>
-                    </div>
-                  </div>
+                  </div> */}
+                  <BottomBar />
                 </div>
               </div>
             ) : (
