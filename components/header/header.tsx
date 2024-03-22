6dcbc302c2a19b54d613714ff75de27f40672ed2
jasmin <jasmikdobariya@gmail.com>
2024-03-22 18:41:48 +0530

    fireLudo

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,17 +1,15 @@
 import Image from 'next/image'
 import React from 'react'
 import logo from '../../public/assets/images/logo.png'
+import header_logo from '../../public/assets/images/header_logo.png'
 
 export default function Header({onLoginClick , onLogoClick }:any) {
 
   return (
-    <header className="fixed top-0 left-0 sm:w-[480px] w-full z-50 bg-white shadow-lg">
-    <div className="container mx-auto px-4 py-3 md:py-4 flex justify-between items-center">
+    <header className="fixed top-[4px] sm:w-[471px] rounded-t-xl w-full  bg-white  shadow-lg">
+    <div className="container mx-auto px-4 py-3 md:py-4 flex justify-center items-center">
       <div className="cursor-pointer" onClick={onLogoClick}>
-        <Image src={logo} width={60} height={60} alt="/" />
-      </div>
-      <div className="border-2 border-blue-700 px-6 py-1 rounded-md">
-        <button onClick={onLoginClick}>Login</button>
+        <Image src={header_logo}  alt="/" />
       </div>
     </div>
   </header>
